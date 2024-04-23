e6632431b6530b2bd26e6b9b7511a14bdab2a528
Adri <131946381+AdriBlt@users.noreply.github.com>
2024-04-23 15:05:57 +0200

    multipli circle

--- src/maths/multiplication-circle/multiplication-circle-game.tsx
+++ src/maths/multiplication-circle/multiplication-circle-game.tsx
@@ -1,16 +1,17 @@
-import { observer } from "mobx-react";
 import * as React from "react";
 import { Button } from "react-bootstrap";
 
 import { NumberInput, NumberInputProps } from "../../shared/number-input";
 import { ProcessingComponent } from "../../shared/processing-component";
 import { getStrings, LocalizedStrings } from "../../strings";
-import { MultiplicationCircleSketch } from "./multiplication-circle-sketch";
+import { CircleProps, DefaultProps, MultiplicationCircleSketch } from "./multiplication-circle-sketch";
 
-@observer
-export class MultiplicationCircleGame extends React.Component {
+export class MultiplicationCircleGame extends React.Component<{}, CircleProps> {
+  public state = DefaultProps;
   private strings: LocalizedStrings = getStrings();
-  private sketch = new MultiplicationCircleSketch();
+  private sketch = new MultiplicationCircleSketch({
+    onPropsChange: (p) => this.setState(p)
+  });
 
   public render() {
     return (
@@ -35,9 +36,9 @@ export class MultiplicationCircleGame extends React.Component {
 
   private getMultiplicatorProps = (): NumberInputProps => {
     return {
-      step: this.sketch.multiplicatorIncrement,
+      step: this.state.multiplicatorIncrement,
       label: this.strings.multiplicationCircle.multiplicator,
-      value: this.sketch.multiplicator,
+      value: this.state.multiplicator,
       onValueChanged: (value: number) => {
         this.sketch.setMultiplicatorValue(value);
       },
@@ -48,9 +49,9 @@ export class MultiplicationCircleGame extends React.Component {
     return {
       step: 0.001,
       label: this.strings.multiplicationCircle.speed,
-      value: this.sketch.multiplicatorIncrement,
+      value: this.state.multiplicatorIncrement,
       onValueChanged: (value: number) => {
-        this.sketch.multiplicatorIncrement = value;
+        this.sketch.setMultiplicatorIncrement(value);
       },
     };
   };
