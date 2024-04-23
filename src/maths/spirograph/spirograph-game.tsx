15ab5368934bc010206251a76ef4f6bca3850cab
Adri <131946381+AdriBlt@users.noreply.github.com>
2024-04-23 15:31:42 +0200

    remove mbx

--- src/maths/spirograph/spirograph-game.tsx
+++ src/maths/spirograph/spirograph-game.tsx
@@ -1,4 +1,3 @@
-import { observer } from "mobx-react";
 import * as React from "react";
 import { ControlBarInput } from "src/shared/control-bar-input";
 
@@ -6,7 +5,6 @@ import { ProcessingComponent } from "../../shared/processing-component";
 import { getStrings, LocalizedStrings } from "../../strings";
 import { SpirographSketch } from "./spirograph-sketch";
 
-@observer
 export class SpirographGame extends React.Component {
   private strings: LocalizedStrings = getStrings();
   private sketch = new SpirographSketch();
